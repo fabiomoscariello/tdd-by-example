59c880cbf8aa8fd836148e90a4b966eccf6ab53b
fabiomoscariello <mf.92@hotmail.it>
2024-05-24 10:15:58 +0200

    feat: added first class with TDD methodology

---
+++ src/main/java/guru/springframework/Dollar.java
@@ -0,0 +1,15 @@
+package guru.springframework;
+
+public class Dollar {
+
+    public int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    public void times(int times) {
+        this.amount *= times;
+    }
+
+}
---
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -0,0 +1,16 @@
+package guru.springframework;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class MoneyTest {
+
+    @Test
+    void testMultiplication(){
+        Dollar myDollar = new Dollar(5);
+        myDollar.times(2);
+        assertEquals(10, myDollar.amount);
+    }
+
+}
